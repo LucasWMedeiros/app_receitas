dbff1a82f2ca6d68e9bb88f91460b2aea70aa2ec
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-25 14:16:20 -0300

    adicionando inkWell para trabalhar com onTap

--- lib/Components/category_item.dart
+++ lib/Components/category_item.dart
@@ -10,20 +10,24 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(category.title!,
-      style: Theme.of(context).textTheme.headline6,),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color!.withOpacity(0.5),
-            category.color!,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-        )
+    return InkWell(
+      onTap: (){},
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(category.title!,
+        style: Theme.of(context).textTheme.headline6,),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color!.withOpacity(0.5),
+              category.color!,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          )
+        ),
       ),
     );
   }
