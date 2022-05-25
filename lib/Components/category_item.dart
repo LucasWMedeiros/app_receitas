66d7cb66a4a0e3d62407034628a8125a3656ca17
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-25 14:19:58 -0300

    adicionando função para acessar a tela de receitas

--- lib/Components/category_item.dart
+++ lib/Components/category_item.dart
@@ -1,33 +1,39 @@
-
+import 'package:app_receitas/Screens/categoris_meals_screen.dart';
 import 'package:app_receitas/models/category.dart';
 import 'package:flutter/material.dart';
 
 class CategoryItem extends StatelessWidget {
-
   final Category category;
 
   CategoryItem(this.category);
 
+  void _selectedCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_){
+          return CategoriesMealsScrean();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
-      onTap: (){},
+      onTap: () => _selectedCategory(context),
       borderRadius: BorderRadius.circular(15),
       child: Container(
         padding: const EdgeInsets.all(15),
-        child: Text(category.title!,
-        style: Theme.of(context).textTheme.headline6,),
+        child: Text(
+          category.title!,
+          style: Theme.of(context).textTheme.headline6,
+        ),
         decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(15),
-          gradient: LinearGradient(
-            colors: [
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(colors: [
               category.color!.withOpacity(0.5),
               category.color!,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight
-          )
-        ),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
       ),
     );
   }
