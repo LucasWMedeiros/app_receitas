28f387ebcf8308257949a48ef6baa19108232b95
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-26 14:07:03 -0300

    alterando apresentação de itens na tela de receitas

--- lib/Screens/categoris_meals_screen.dart
+++ lib/Screens/categoris_meals_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
+import 'package:app_receitas/Data/dummy_data.dart';
 import 'package:app_receitas/models/category.dart';
 import 'package:flutter/material.dart';
 
@@ -8,13 +9,22 @@ class CategoriesMealsScrean extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories!.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title!),
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Receitas por Categoria'),
+        child: ListView.builder(
+          itemCount: categoryMeals.length,
+          itemBuilder: ((ctx, index) {
+            return Text(categoryMeals[index].title!);
+          })),
       ),
     );
   }
