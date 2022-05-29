8c69d228e44acd0205d6e62602cf6805c0aae50b
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-29 10:34:24 -0300

    configurando tela detalhes da refeição

--- lib/Screens/meal_detail_screen.dart
+++ lib/Screens/meal_detail_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
 
 
 import 'package:app_receitas/models/meal.dart';
@@ -17,8 +17,50 @@ class MealDetailScreen extends StatelessWidget {
         title: Text(meal.title!),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text('Detalhe da Refeição'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(
+              meal.imageUrl!,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'Ingredientes',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 250,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: ListView.builder(
+              itemCount: meal.ingredients!.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10
+                    ),
+                    child: Text(meal.ingredients![index]),
+                  ),
+                  color: Theme.of(context).accentColor,
+                );
+              },
+                 ),
+          )
+        ],
       ),
     );
   }
