061d31dba1d14dd8df51768567c0b3a1578818eb
LucasWMedeiros <66040430+LucasWMedeiros@users.noreply.github.com>
2022-05-31 13:56:14 -0300

    configurando apresentação de passo-a-passo do preparo

--- lib/Screens/meal_detail_screen.dart
+++ lib/Screens/meal_detail_screen.dart
@@ -1,66 +1,86 @@
 // ignore_for_file: prefer_const_constructors, deprecated_member_use
 
-
 import 'package:app_receitas/models/meal.dart';
 import 'package:flutter/material.dart';
 
 class MealDetailScreen extends StatelessWidget {
 
+  _createSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget _createSectionContainer(Widget child){
+    return Container(
+            width: 300,
+            height: 250,
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10)),
+                child: child,
+                );
+  }
 
   @override
   Widget build(BuildContext context) {
-  
-  final meal = ModalRoute.of(context)!.settings.arguments as Meal;
+    final meal = ModalRoute.of(context)!.settings.arguments as Meal;
 
     return Scaffold(
       appBar: AppBar(
         title: Text(meal.title!),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              meal.imageUrl!,
-              fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl!,
+                fit: BoxFit.cover,
+              ),
             ),
-          ),
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingredientes',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-          ),
-          Container(
-            width: 300,
-            height: 250,
-            padding: EdgeInsets.all(10),
-            margin: EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10)
-            ),
-            child: ListView.builder(
-              itemCount: meal.ingredients!.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 10
+              _createSectionTitle(context, 'Ingredientes'),
+              _createSectionContainer(
+               ListView.builder(
+                itemCount: meal.ingredients!.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Padding(
+                      padding:
+                          const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                      child: Text(meal.ingredients![index]),
                     ),
-                    child: Text(meal.ingredients![index]),
-                  ),
-                  color: Theme.of(context).accentColor,
-                );
-              },
-                 ),
-          )
-        ],
+                    color: Theme.of(context).accentColor,
+                  );
+                },
+              ),
+            ),
+            _createSectionTitle(context, 'Modo de Preparo'),
+            _createSectionContainer(
+              ListView.builder(
+                itemCount: meal.steps!.length,
+                itemBuilder: (ctx, index) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      child: Text('${index + 1}'),
+                    ),
+                    title: Text(meal.steps![index]),
+                  );
+                },
+              )
+            )
+          ],
+        ),
       ),
     );
   }
